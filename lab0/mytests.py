e9104e15355e62ba251a27c08dd0fe8b15e6e36f
Mekaiten <uriya.mekaiten@intel.com>
2019-03-23 15:31:16 +0200

    starting the algebra part

--- lab0/mytests.py
+++ lab0/mytests.py
@@ -1,4 +1,5 @@
 from lab0 import *
+from algebra import *
 
 
 def tst_count_pattern():
@@ -21,6 +22,31 @@ def tst_depth():
         print(depth(expr))
 
 
+def tst_tree_ref():
+
+    tree =      [1,     [2, [ 3,4,5, [6,7] ],   8]]
+    index =     [1,1,3,1]
+
+    print (tree_ref(tree, index))
+
+
+def tst_algebra():
+
+    # check Sums:
+    cur_sum = Sum([1, Sum([2, Sum([Sum([3, 4]), Sum([5, 6])])])])
+    print( "cur_sum: \t\t\t\t" + str(cur_sum) )
+
+    flat_sum = cur_sum.flatten()
+    print( "flat_sum: \t\t\t\t" + str(flat_sum))
+
+    print("simplified orig sum: \t" + str(cur_sum.simplify()))
+    print("simplified flat sum: \t" + str(flat_sum.simplify()))
+
+    # check Products:
+    
+
 if __name__ == "__main__":
     #tst_count_pattern()
-    tst_depth()
+    #tst_depth()
+    #tst_tree_ref()
+    tst_algebra()
